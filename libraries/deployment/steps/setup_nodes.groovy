4b98c02f93c116850f88764d5ffd1053cb12379b
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-01-11 16:12:08 +0530

    Added additional libraries

---
+++ libraries/deployment/steps/setup_nodes.groovy
@@ -0,0 +1,5 @@
+void call(){
+    stage("Deployment: Setup Nodes"){
+        println "Nodes are prepared for deployment"
+    }
+}
---
+++ libraries/infrastructure/steps/setup_env.groovy
@@ -0,0 +1,5 @@
+void call(){
+    stage("Deployment: Setup Environment"){
+        println "All required python packages are installed"
+    }
+}
