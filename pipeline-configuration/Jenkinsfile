8ce73e30ff52be55876cae62dd1e918e94536f59
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-01-11 16:57:47 +0530

    Added application environments

--- pipeline-configuration/Jenkinsfile
+++ pipeline-configuration/Jenkinsfile
@@ -1,1 +1,3 @@
 continuous_integration()
+deploy_to dev
+deploy_to prod
--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -9,3 +9,13 @@ stages{
         setup_env
     }
 }
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
+}
